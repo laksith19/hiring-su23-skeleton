9a092cb2e39719d1bd5fe8445ff40b96994700b4
laksith19 <laksith@berkeley.edu>
2023-04-09 18:07:19 -0700

    Add skeleton code

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# CS61BL Summer 2023 Hiring - OH Simulation 
+
+This repo contains code that will be used for round 1 of CS61BL Summer 2023 interviews. 
+
+## Setup Intructions
+
---
+++ tests/MinHeapTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MinHeapTest {
+
+    @Test
+    public void test1() {
+        assertEquals(1,2);
+    }
+}
